d119cefbc0ed213f804b7a1c13a7180a5419f41d
derverdox <lukasjonsson@outlook.de>
2023-01-20 18:27:58 +0100

    Added new Wall Hiding Script / Transparency Script

---
+++ GWG/Assets/RoomTransparency/WallInit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace RoomTransparency
+{
+    public class WallInit : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Destroy(gameObject.GetComponent<WallInit>());
+            var originalParent = transform.parent;
+            
+            var newParent = new GameObject();
+            newParent.transform.SetParent(originalParent);
+            var wall = newParent.AddComponent<HideableWall>();
+            
+            newParent.transform.name = transform.name;
+            transform.parent = newParent.transform;
+            transform.name = "Opaque Material";
+
+            wall.opaqueOriginal = transform.gameObject;
+            Destroy(wall.opaqueOriginal.GetComponent<WallInit>());
+
+            wall.transparentMaterial = Instantiate(wall.opaqueOriginal);
+            Destroy(wall.transparentMaterial.GetComponent<WallInit>());
+            
+            wall.transparentMaterial.name = "TransparentMaterial";
+            wall.transparentMaterial.transform.SetParent(wall.transform);
+
+            foreach (var material in wall.transparentMaterial.GetComponent<Renderer>().materials)
+            {
+                material.SetBlendMode(Extensions.BlendMode.Transparent);
+                material.SetFloat("_Surface", (float)BaseShaderGUI.SurfaceType.Transparent);
+                Extensions.ChangeAlpha(material, 0.15f);
+            }
+
+            wall.transparentMaterial.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
